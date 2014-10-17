54dab0bfaed9038c5b3003cbe427981d6f319d36
Nick Dyer <nbdyer@gmail.com>
2014-10-17 12:42:42 +0100

    Create nested hash (solves exerices 4.3)

--- rails-flavored-ruby-exercises.rb
+++ rails-flavored-ruby-exercises.rb
@@ -2,11 +2,21 @@ def string_shuffle(s)
 	s.split('').shuffle.join('')
 end
 
-puts string_shuffle("foobar")
-
 
 class String
 	def shuffle
 		self.split('').shuffle.join
 	end
 end
+
+
+person1 = {first: "Barry", last: "Dyer"}
+person2 = {first: "Carol", last: "Dyer"}
+person3 = {first: "Nick", last: "Dyer"}
+
+params = {father: person1, mother: person2, child: person3}
+
+puts params.inspect,
+
+params[:father][:first],
+params[:child][:last]
